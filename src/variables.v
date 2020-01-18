fd1c78929ef9fb9538652aba45bd9a66f77dcb75
Bruno Silva <bruno3dcontato@gmail.com>
2020-01-18 12:34:42 -0300

    adicionado lista dos tipos nativos de variaveis

--- src/variables.v
+++ src/variables.v
@@ -1,3 +1,15 @@
+/*
+Basic Types
+bool
+string
+i8    i16  int  i64      i128 (soon)
+byte  u16  u32  u64      u128 (soon)
+rune // represents a Unicode code point 
+f32 f64
+byteptr
+voidptr
+*/
+
 // variaveis so devem ser declaradas e utilizadas
 // dentro de seus reespectiovos escopos
 // nao ha declaracao para uso "global"
